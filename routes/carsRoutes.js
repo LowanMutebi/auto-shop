8b18855d8afe8124a1984583078bc50fe3edce86
magalaReuben <magalareuben60@gmail.com>
2024-12-07 12:42:56 +0300

    all

--- routes/carsRoutes.js
+++ routes/carsRoutes.js
@@ -1,11 +1,11 @@
 const fetchedCars = require('../cars.js');
 
 module.exports = app => {
-  app.get('/api/cars',(req, res) => {
+  app.get('https://auto-shop-server-xi.vercel.app/api/cars',(req, res) => {
     res.send(fetchedCars);
   });
 
-  app.post('/api/carfilter',(req,res)=>{
+  app.post('https://auto-shop-server-xi.vercel.app/api/carfilter',(req,res)=>{
     console.log('**********************************');
     console.log(req.body);
     console.log('**********************************');
--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,13 +1,13 @@
 const fetchedProducts = require('../products.js');
 
 module.exports = app => {
-  app.get('/api/products/:category',(req, res) => {
+  app.get('https://auto-shop-server-xi.vercel.app/api/products/:category',(req, res) => {
     res.send(
       fetchedProducts.filter((product)=>product.category == (req.url.split(':')[1]))
     );
   });
 
-  app.get('/api/product/:id',(req, res) => {    
+  app.get('https://auto-shop-server-xi.vercel.app/api/product/:id',(req, res) => {    
     res.send(
       fetchedProducts.filter((product)=>product._id==req.url.split(':')[1])[0]
     );
