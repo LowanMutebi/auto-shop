761d6a0dc399a1f246b36e5e6c946e742fa3f06f
magalaReuben <magalareuben60@gmail.com>
2024-12-07 10:26:14 +0300

    new changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 require('./routes/productsRoutes')(app);
 require('./routes/carsRoutes')(app);
 
-if (process.env.NODE_ENV === 'production') {
+if (true) {
   // Express will serve up production assets
   // like our main.js file, or main.css file!
   app.use(express.static('client/build'));
