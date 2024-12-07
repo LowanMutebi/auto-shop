a4dc3b792f7163cbe5671f8d24c5de0e566c8fc9
magalaReuben <magalareuben60@gmail.com>
2024-12-07 12:53:02 +0300

    all

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const cors = require('cors')
 
 app.use(bodyParser.json());
 app.use(cors())
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://auto-shop-frontend.vercel.app/"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 
 
